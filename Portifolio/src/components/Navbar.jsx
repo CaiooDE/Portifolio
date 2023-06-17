0cc8769efbe49d55e3924b65d5e0c58ac467670d
CaiooDE <caiode@id.uff.br>
2023-06-17 12:43:34 -0300

    NavBar quase finalizada

--- Portifolio/src/components/Navbar.jsx
+++ Portifolio/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import styles from './Navbar.module.css';
-import logoCaio from '../assets/logoCaio.svg'
+import logoCaio from '../assets/logoCaio.png';
 
 export function Navbar() {
-  return(
+  return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
         <img src={logoCaio} alt="Logo do Desenvolvedor" />
       </div>
-
-      <a href="#">Inicio</a>
-      <a href="#">Projetos</a>
-      <a href="#">Contato</a>
+      <div className={styles.ancoras}>
+        <a href="#">Inicio</a>
+        <a href="#">Projetos</a>
+        <a href="#">Contato</a>
+      </div>
     </div>
-  )
+  );
 }
