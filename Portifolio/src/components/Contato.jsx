518c7138fd5d8c3ff8c34f129190d9b36c4b05bd
CaiooDE <caiode@id.uff.br>
2023-06-19 16:48:00 -0300

    Pagina de contato finalizada o front

--- Portifolio/src/components/Contato.jsx
+++ Portifolio/src/components/Contato.jsx
@@ -1,18 +1,65 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Contato.module.css';
+import { FaWhatsapp } from 'react-icons/fa';
 
 export function Contato() {
+  const [alertText, setAlertText] = useState('');
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  const handleCopyPhoneNumber = () => {
+    const phoneNumber = '+55 (21) 98997-0805';
+    navigator.clipboard.writeText(phoneNumber);
+    setAlertText('Copiado para a área de transferência!');
+    setAlertVisible(true);
+    setTimeout(() => {
+      setAlertVisible(false);
+    }, 2000);
+  };
+
+  const floatingAlertStyle = {
+    position: 'fixed',
+    top: mousePosition.y,
+    left: mousePosition.x,
+    display: alertVisible ? 'block' : 'none',
+  };
+
   return (
-    <div id='Contato' className={styles.container}>
+    <div id="Contato" className={styles.container}>
       <div className={styles.content2}>
         <h1>SE INTERESSOU PELO CONTEUDO?</h1>
-        <h2>Nao perca a oportunidade</h2>
-        <strong>Entre em contato pelos links no Menu de Navegação ou envie um email diretamente</strong>
+        <h2>Não perca a oportunidade</h2>
+        <strong>
+          Entre em contato pelos links no Menu de Navegação ou envie um email diretamente para{' '}
+          <a className={styles.mailtext} href="mailto:cderrecaio@gmail.com">
+            cderrecaio@gmail.com
+          </a>
+        </strong>
+        <div className={styles.contactInfo}>
+          <p onClick={handleCopyPhoneNumber}>
+            <strong>
+              <FaWhatsapp size={20} /> {' '}Telefone:
+            </strong>{' '}
+            +55 (21) 98997-0805
+          </p>
+        </div>
       </div>
 
       <div className={styles.content1}>
-        <h2 >Entre em contato</h2>
+        <h2>Entre em contato</h2>
         <form action="enviar_email.php" method="POST">
           <div className={styles.formgroup}>
             <label htmlFor="name">Nome</label>
@@ -27,9 +74,15 @@ export function Contato() {
             <textarea id="message" name="message" required></textarea>
           </div>
           <div className={styles.formgroup}>
-            <input type="submit" className="btn" value="Enviar" />
+            <input type="submit" className={styles.btn} value="Enviar" />
           </div>
         </form>
+      </div>
+
+      <div className={styles.floatingAlert} style={floatingAlertStyle}>
+        <div className={styles.infoBalloon}>
+          {alertText}
+        </div>
       </div>
     </div>
   );
